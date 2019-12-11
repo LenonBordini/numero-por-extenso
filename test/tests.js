5954b6ae74c6d2e215dc4d7b89fe958e1412a1a9
Lenon Bordini <lenon@engsolutions.com.br>
2019-12-11 13:56:44 -0300

    ajustando indentação

--- test/tests.js
+++ test/tests.js
@@ -17,32 +17,38 @@ describe('testando estilo normal', () => {
         ['zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove']
             .forEach((n, i) => assert.equal(numero.porExtenso(i), n));
     });
+
     test('escrevendo do 10 ao 19', () => {
         ['dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove']
             .forEach((n, i) => assert.equal(numero.porExtenso(i + 10), n))
     });
+
     test('escrevendo dezenas', () => {
         ['dez', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa']
             .forEach((n, i) => assert.equal(numero.porExtenso((i + 1) * 10), n))
     });
+
     test('escrevendo centenas', () => {
         ['dez', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa']
             .forEach((n, i) => assert.equal(numero.porExtenso((i + 1) * 10), n))
-    });   
-    test('escrevendo milhares', () => {
-        assert.equal(numero.porExtenso(1000), "um mil");
-        assert.equal(numero.porExtenso(2000), "dois mil");
-        assert.equal(numero.porExtenso(10000), "dez mil");
-        assert.equal(numero.porExtenso(20000), "vinte mil");
     });
+
+    test('escrevendo milhares', () => {
+        assert.equal(numero.porExtenso(1000), 'um mil');
+        assert.equal(numero.porExtenso(2000), 'dois mil');
+        assert.equal(numero.porExtenso(10000), 'dez mil');
+        assert.equal(numero.porExtenso(20000), 'vinte mil');
+    });
+
     test('escrevendo centenas de milhares', () => {
         ['cem', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos', 'oitocentos', 'novecentos']
             .forEach((n, i) => assert.equal(numero.porExtenso(((i + 1) * 100000)), n + ' mil'));
     });
 
     test('escrevendo números negativos', () => {
-        assert.equal(numero.porExtenso(-1), "menos um");
-        assert.equal(numero.porExtenso(-200), "menos duzentos");
+        assert.equal(numero.porExtenso(-1), 'menos um');
+        assert.equal(numero.porExtenso(-200), 'menos duzentos');
+        assert.equal(numero.porExtenso(-0.5), 'menos zero vírgula cinco');
     });
 });
 
@@ -51,12 +57,12 @@ describe('testando README.md', () => {
         assert.equal(numero.porExtenso(1), 'um');
         assert.equal(numero.porExtenso(1, numero.estilo.monetario), 'um real');
         assert.equal(numero.porExtenso(1, numero.estilo.porcentagem), 'um porcento');
-    
+
         assert.equal(numero.porExtenso(1234.50), 'um mil duzentos e trinta e quatro vírgula cinco');
         assert.equal(numero.porExtenso('1234.50'), 'um mil duzentos e trinta e quatro vírgula cinquenta');
-        
+
         assert.equal(numero.porExtenso(1234.5, numero.estilo.monetario), 'um mil duzentos e trinta e quatro reais e cinquenta centavos');
-    
+
         assert.equal(numero.porExtenso(1234.50, numero.estilo.porcentagem), 'um mil duzentos e trinta e quatro vírgula cinco porcento');
         assert.equal(numero.porExtenso('1234.50', numero.estilo.porcentagem), 'um mil duzentos e trinta e quatro vírgula cinquenta porcento');
     });
